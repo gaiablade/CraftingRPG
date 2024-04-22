446a066e6ef37db2ac17a264d4408eadc99223a8
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-04-22 14:08:35 -0400

    Change tileset image sources

--- CraftingRPG/Tmx/objects_32x16.tsx
+++ CraftingRPG/Tmx/objects_32x16.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="objects_32x16" tilewidth="32" tileheight="16" tilecount="4" columns="2">
- <image source="../bin/Debug/net6.0/Tmx/img/objects_32x16.png" width="64" height="32"/>
+ <image source="img/objects_32x16.png" width="64" height="32"/>
 </tileset>
--- CraftingRPG/Tmx/objects_48x64.tsx
+++ CraftingRPG/Tmx/objects_48x64.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="objects_48x48" tilewidth="48" tileheight="64" tilecount="4" columns="2">
- <image source="../bin/Debug/net6.0/Tmx/img/objects_48x48.png" width="96" height="128"/>
+ <image source="img/objects_48x48.png" width="96" height="128"/>
 </tileset>
