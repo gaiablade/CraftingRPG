998ec4ad43bab52e9aa5ca9e6f3c2ed7d46e58a6
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-04-25 19:50:24 -0400

    Added animations to PlayerInstance, moved some logic into PlayerInstance from MapManager

--- CraftingRPG/Entities/PlayerInstance.cs
+++ CraftingRPG/Entities/PlayerInstance.cs
@@ -1,6 +1,6 @@
-﻿using System.Drawing;
-using CraftingRPG.Constants;
+﻿using CraftingRPG.Constants;
 using CraftingRPG.Interfaces;
+using CraftingRPG.SpriteAnimation;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended;
 using Point = Microsoft.Xna.Framework.Point;
@@ -13,28 +13,66 @@ public class PlayerInstance : IInstance
 {
     public PlayerInfo Info { get; set; }
 
-    #region Movement
-
+    #region Constants
     public const float MovementSpeed = 2F;
     public const int AttackFrameLength = 5;
     public static readonly Point SpriteSize = new Point(48, 48);
-    
-    public Vector2 Position = new Vector2();
     public Vector2 Size = new Vector2(48, 48);
+    #endregion
+    
+    #region Public Getters/Setters
+    public Vector2 Position = new Vector2();
+
     public int FacingDirection { get; set; } = Direction.Down;
-    public bool IsAttacking { get; set; } = false;
-    public int IdleOrWalkingAnimFrames { get; set; } = 0;
-    public int AttackAnimFrames { get; set; } = 0;
-    public bool IsWalking { get; set; } = false;
+    public bool IsAttacking { get; set; }
+    public bool IsWalking { get; set; }
+    #endregion
+    
     public Vector2 MovementVector;
     public bool IsAboveDrop = false;
     public RectangleF AttackRect;
 
+    
+    #region Animation
+    private int PreviousFacingDirection;
+    private int CurrentFacingDirection = Direction.Down;
+    
+    private bool PreviousIsAttacking = false;
+    private bool CurrentIsAttacking = false;
+    
+    private bool PreviousIsWalking;
+    private bool CurrentIsWalking;
+    
+    private Animation IdleSouthAnimation;
+    private Animation IdleNorthAnimation;
+    private Animation IdleSideAnimation;
+    private Animation WalkingSouthAnimation;
+    private Animation WalkingNorthAnimation;
+    private Animation WalkingSideAnimation;
+    private Animation AttackingSouthAnimation;
+    private Animation AttackingNorthAnimation;
+    private Animation AttackingSideAnimation;
+
+    private Animation CurrentAnimation;
     #endregion
 
     public PlayerInstance(PlayerInfo info)
     {
+        const double idleInterval = 0.1;
+        const double walkInterval = 0.1;
+        const double attackInterval = 0.1;
+        
         Info = info;
+        IdleSouthAnimation = new Animation(6, idleInterval, SpriteSize, true, 0, 0);
+        IdleSideAnimation = new Animation(6, idleInterval, SpriteSize, true, 0, 48);
+        IdleNorthAnimation = new Animation(6, idleInterval, SpriteSize, true, 0, 96);
+        WalkingSouthAnimation = new Animation(6, walkInterval, SpriteSize, true, 0, 144);
+        WalkingSideAnimation = new Animation(6, walkInterval, SpriteSize, true, 0, 192);
+        WalkingNorthAnimation = new Animation(6, walkInterval, SpriteSize, true, 0, 240);
+        AttackingSouthAnimation = new Animation(4, attackInterval, SpriteSize, false, 0, 288);
+        AttackingSideAnimation = new Animation(4, attackInterval, SpriteSize, false, 0, 336);
+        AttackingNorthAnimation = new Animation(4, attackInterval, SpriteSize, false, 0, 384);
+        CurrentAnimation = IdleSouthAnimation;
     }
 
     public Vector2 GetPosition() => Position;
@@ -47,8 +85,128 @@ public class PlayerInstance : IInstance
 
     public int GetSpriteSheetIndex() => SpriteIndex.Player1;
 
-    public RectangleF GetCollisionBox() =>
-        new RectangleF(new Point2(Position.X + 18, Position.Y + 22), new Size2(13, 19));
+    public RectangleF GetCollisionBox() => new(new Point2(Position.X + 18, Position.Y + 22), new Size2(13, 19));
 
     public Vector2 SetPosition(Vector2 position) => Position = position;
+
+    public void UpdateAnimation(GameTime gameTime)
+    {
+        if (IsAttacking && CurrentAnimation.IsAnimationOver())
+        {
+            IsAttacking = false;
+        }
+        
+        PreviousFacingDirection = CurrentFacingDirection;
+        CurrentFacingDirection = FacingDirection;
+
+        PreviousIsAttacking = CurrentIsAttacking;
+        CurrentIsAttacking = IsAttacking;
+
+        PreviousIsWalking = CurrentIsWalking;
+        CurrentIsWalking = IsWalking;
+        
+        var attackChanged = CurrentIsAttacking != PreviousIsAttacking;
+        var walkChanged = CurrentIsWalking != PreviousIsWalking;
+        var directionChanged = CurrentFacingDirection != PreviousFacingDirection;
+        
+        if (attackChanged || walkChanged || directionChanged)
+        {
+            SetAnimation();
+        }
+        
+        CurrentAnimation.Update(gameTime);
+
+        if (IsAttacking)
+        {
+            CalculateAttackRectangle();
+        }
+    }
+
+    private void SetAnimation()
+    {
+        CurrentAnimation.Reset();
+        if (CurrentIsAttacking)
+        {
+            CurrentAnimation = CurrentFacingDirection switch
+            {
+                Direction.Down => AttackingSouthAnimation,
+                Direction.Up => AttackingNorthAnimation,
+                _ => AttackingSideAnimation
+            };
+        }
+        else if (CurrentIsWalking)
+        {
+            CurrentAnimation = CurrentFacingDirection switch
+            {
+                Direction.Down => WalkingSouthAnimation,
+                Direction.Up => WalkingNorthAnimation,
+                _ => WalkingSideAnimation
+            };
+        }
+        else
+        {
+            CurrentAnimation = CurrentFacingDirection switch
+            {
+                Direction.Down => IdleSouthAnimation,
+                Direction.Up => IdleNorthAnimation,
+                _ => IdleSideAnimation
+            };
+        }
+    }
+
+    private void CalculateAttackRectangle()
+    {
+        var animFrame = CurrentAnimation.GetCurrentFrame();
+        var pPos = Position.ToPoint();
+        
+        if (FacingDirection == Direction.Left)
+        {
+            AttackRect = animFrame switch
+            {
+                0 => new Rectangle(new Point(0, 0), new Point(0, 0)),
+                1 => new Rectangle(new Point(pPos.X + 6, pPos.Y + 33), new Point(13, 14)),
+                2 => new Rectangle(0, 0, 0, 0),
+                3 => new Rectangle(0, 0, 0, 0),
+                _ => AttackRect
+            };
+        }
+        else if (FacingDirection == Direction.Right)
+        {
+            AttackRect = animFrame switch
+            {
+                0 => new Rectangle(new Point(0, 0), new Point(0, 0)),
+                1 => new Rectangle(new Point(pPos.X + 32, pPos.Y + 32), new Point(13, 14)),
+                2 => new Rectangle(0, 0, 0, 0),
+                3 => new Rectangle(0, 0, 0, 0),
+                _ => AttackRect
+            };
+        }
+        else if (FacingDirection == Direction.Down)
+        {
+            AttackRect = animFrame switch
+            {
+                0 => new Rectangle(new Point(0, 0), new Point(0, 0)),
+                1 => new Rectangle(pPos.X + 17, pPos.Y + 37, 22, 11),
+                2 => new Rectangle(0, 0, 0, 0),
+                3 => new Rectangle(0, 0, 0, 0),
+                _ => AttackRect
+            };
+        }
+        else if (FacingDirection == Direction.Up)
+        {
+            AttackRect = animFrame switch
+            {
+                0 => new Rectangle(new Point(0, 0), new Point(0, 0)),
+                1 => new Rectangle(pPos.X + 11, pPos.Y + 22, 22, 11),
+                2 => new Rectangle(0, 0, 0, 0),
+                3 => new Rectangle(0, 0, 0, 0),
+                _ => AttackRect
+            };
+        }
+    }
+
+    public Rectangle GetSourceRectangle()
+    {
+        return CurrentAnimation.GetSourceRectangle();
+    }
 }
