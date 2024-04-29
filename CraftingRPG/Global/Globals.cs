3b73971dc46615054fcd5af88949d39f797e5e9e
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-04-29 11:56:00 -0400

    Changed Globals class from a singleton to a static class, removed unused fonts, moved PlayerSpriteSheet to Assets

--- CraftingRPG/Global/Globals.cs
+++ CraftingRPG/Global/Globals.cs
@@ -4,19 +4,9 @@ using MonoGame.Extended;
 
 namespace CraftingRPG.Global;
 
-public class Globals
+public static class Globals
 {
-    public static readonly Globals Instance = new();
-
-    public PlayerInstance Player { get; set; }
-    public bool ActionKeyPressed { get; set; } = false;
-    public OrthographicCamera Camera { get; set; }
-
-    // MonoGame
-    public SpriteFont DefaultFont { get; set; }
-    public SpriteFont Fnt10 { get; set; }
-    public SpriteFont Fnt12 { get; set; }
-    public SpriteFont Fnt15 { get; set; }
-    public SpriteFont Fnt20 { get; set; }
-    public Texture2D PlayerSpriteSheet { get; set; }
+    public static PlayerInstance Player { get; set; }
+    public static  bool ActionKeyPressed { get; set; } = false;
+    public static  OrthographicCamera Camera { get; set; }
 }
