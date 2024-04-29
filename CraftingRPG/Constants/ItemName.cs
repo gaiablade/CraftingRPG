570b86b09f887988baf01ab37466d6bb9d2463fe
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-04-29 12:40:14 -0400

    Add ItemName class

---
+++ CraftingRPG/Constants/ItemName.cs
@@ -0,0 +1,16 @@
+namespace CraftingRPG.Constants;
+
+public static class ItemName
+{
+    public const string ArcaneFlower = "Arcane Flower";
+    public const string EmptyBottle = "Empty Bottle";
+    public const string HealingMushroom = "Healing Mushroom";
+    public const string HeartyFlower = "Hearty Flower";
+    public const string IronBand = "Iron Band";
+    public const string IronChunk = "Iron Chunk";
+    public const string IronHelmet = "Iron Helmet";
+    public const string IronSword = "Iron Sword";
+    public const string MageBracelet = "Mage Bracelet";
+    public const string MediumHealthPotion = "Medium Health Potion";
+    public const string SmallHealthPotion = "Small Health Potion";
+}
