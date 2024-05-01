ea1de012ddba275dcb137950f1ebf19065e5a12e
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-05-01 12:13:18 -0400

    Dev/map edits (#24)
    * Throw exception to help identify objects without the "object_type" property
    
    * Add stone path terrain brush
    
    * Added fence tileset and object data for fences and new tree type
    
    * Changed default map
    
    * Added content
    
    * Added enemy_template.tx
    
    * Added fence png to content
    
    * Version no.

--- CraftingRPG/Tmx/plains.tsx
+++ CraftingRPG/Tmx/plains.tsx
@@ -20,5 +20,23 @@
    <wangtile tileid="16" wangid="0,1,0,0,0,1,0,0"/>
    <wangtile tileid="17" wangid="0,0,0,1,0,0,0,1"/>
   </wangset>
+  <wangset name="stone path" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="49" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="50" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="52" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="53" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="55" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="56" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="57" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="58" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="59" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="62" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="63" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="64" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="65" wangid="0,0,0,1,0,0,0,1"/>
+  </wangset>
  </wangsets>
 </tileset>
