807d5b0e49dd82d45fad7e90002a9b8d38c6d961
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-05-02 20:22:57 -0400

    Other changes

--- CraftingRPG/GameManager.cs
+++ CraftingRPG/GameManager.cs
@@ -1,40 +1,34 @@
-﻿using CraftingRPG.Entities;
-using CraftingRPG.Enums;
-using CraftingRPG.Interfaces;
-using CraftingRPG.Items;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Collections.Generic;
-using CraftingRPG.AssetManagement;
+﻿using CraftingRPG.AssetManagement;
+using CraftingRPG.Entities;
 using CraftingRPG.GameStateManagement;
-using CraftingRPG.GameStateManagement.States;
+using CraftingRPG.GameStateManagement.GameStates;
 using CraftingRPG.Global;
 using CraftingRPG.InputManagement;
 using CraftingRPG.MapManagement;
 using CraftingRPG.SoundManagement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace CraftingRPG
 {
     public class GameManager : Game
     {
-        private GraphicsDeviceManager _graphics;
+        private readonly GraphicsDeviceManager Graphics;
         public static SpriteBatch SpriteBatch { get; private set; }
-        public static KeyboardState @KeyboardState { get; private set; }
-        public static Texture2D SpriteSheet { get; private set; }
-        public static Texture2D TileSet { get; private set; }
+        private static KeyboardState @KeyboardState { get; set; }
         public static Texture2D Pixel { get; private set; }
         public static Point Resolution { get; private set; }
+        public static Rectangle WindowBounds { get; private set; }
         public static Point ScreenCenter => Vector2.Divide(Resolution.ToVector2(), 2).ToPoint();
-        public static PlayerInfo PlayerInfo { get; private set; }
-        public static Dictionary<ItemId, IItem> ItemInfo { get; private set; }
+        // public static Dictionary<ItemId, IItem> ItemInfo { get; private set; }
         public static Point PlayerSpriteSize = new Point(48, 48);
 
         public static GameStateManager StateManager { get; private set; } = GameStateManager.Instance;
 
         public GameManager()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -42,31 +36,33 @@ namespace CraftingRPG
         protected override void Initialize()
         {
             Resolution = new Point(800, 608);
-            _graphics.PreferredBackBufferWidth = Resolution.X;
-            _graphics.PreferredBackBufferHeight = Resolution.Y;
-            _graphics.ApplyChanges();
+            Graphics.PreferredBackBufferWidth = Resolution.X;
+            Graphics.PreferredBackBufferHeight = Resolution.Y;
+            Graphics.ApplyChanges();
             Resolution = new Point(GraphicsDevice.PresentationParameters.BackBufferWidth,
                 GraphicsDevice.PresentationParameters.BackBufferHeight);
+            WindowBounds = new Rectangle(Point.Zero, Resolution);
 
             Pixel = new Texture2D(GraphicsDevice, 1, 1);
             Pixel.SetData(new[] { Color.White });
 
-            ItemInfo = new Dictionary<ItemId, IItem>
-            {
-                { ItemId.EmptyBottle, new EmptyBottleItem() },
-                { ItemId.HealingMushroom, new HealingMushroomItem() },
-                { ItemId.HeartyFlower, new HeartyFlowerItem() },
-                { ItemId.SmallHealthPotion, new SmallHealthPotionItem() },
-                { ItemId.MediumHealthPotion, new MediumHealthPotionItem() },
-                { ItemId.IronSword, new IronSwordItem() },
-                { ItemId.IronChunk, new IronChunkItem() },
-                { ItemId.IronHelmet, new IronHelmetItem() },
-                { ItemId.IronBand, new IronBandItem() },
-                { ItemId.ArcaneFlower, new ArcaneFlowerItem() },
-                { ItemId.MageBracelet, new MageBraceletItem() },
-            };
+            // ItemInfo = new Dictionary<ItemId, IItem>
+            // {
+            //     { ItemId.EmptyBottle, new EmptyBottleItem() },
+            //     { ItemId.HealingMushroom, new HealingMushroomItem() },
+            //     { ItemId.HeartyFlower, new HeartyFlowerItem() },
+            //     { ItemId.SmallHealthPotion, new SmallHealthPotionItem() },
+            //     { ItemId.MediumHealthPotion, new MediumHealthPotionItem() },
+            //     { ItemId.IronSword, new IronSwordItem() },
+            //     { ItemId.IronChunk, new IronChunkItem() },
+            //     { ItemId.IronHelmet, new IronHelmetItem() },
+            //     { ItemId.IronBand, new IronBandItem() },
+            //     { ItemId.ArcaneFlower, new ArcaneFlowerItem() },
+            //     { ItemId.MageBracelet, new MageBraceletItem() },
+            // };
 
-            PlayerInfo = new PlayerInfo();
+            Globals.PlayerInfo = new PlayerInfo();
+            Globals.Player = new PlayerInstance(Globals.PlayerInfo);
             
             InputManager.Instance.LoadKeybindingConfiguration();
 
@@ -76,13 +72,11 @@ namespace CraftingRPG
         protected override void LoadContent()
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
-            SpriteSheet = Content.Load<Texture2D>("textures/crpg_spritesheet");
-            TileSet = Content.Load<Texture2D>("textures/crpg_tileset");
 
             MapManager.Instance.LoadMapsFromContents(this.Content);
             Assets.Instance.LoadAssets(this.Content);
 
-            StateManager.PushState<OverworldState>(true);
+            StateManager.PushState<OverWorldGameState>(true);
         }
 
         protected override void Update(GameTime gameTime)
@@ -99,7 +93,7 @@ namespace CraftingRPG
             InputManager.Instance.Update(KeyboardState, gameTime);
 
             StateManager.ProcessStateRequests();
-            StateManager.CurrentState.Update(gameTime);
+            StateManager.CurrentGameState.Update(gameTime);
 
             base.Update(gameTime);
         }
--- CraftingRPG/QuestManagement/QuestBook.cs
+++ CraftingRPG/QuestManagement/QuestBook.cs
@@ -6,17 +6,38 @@ namespace CraftingRPG.QuestManagement;
 public class QuestBook
 {
     private IList<IQuestInstance> ActiveQuests { get; set; }
+    private IList<FetchQuestInstance> FetchQuests { get; set; }
+    private IList<DefeatEnemyQuestInstance> DefeatEnemyQuests { get; set; }
+    private IList<CraftQuestInstance> CraftQuests { get; set; }
 
     public IList<IQuestInstance> GetActiveQuests() => ActiveQuests;
+    public IList<FetchQuestInstance> GetFetchQuests() => FetchQuests;
+    public IList<DefeatEnemyQuestInstance> GetDefeatEnemyQuests() => DefeatEnemyQuests;
+    public IList<CraftQuestInstance> GetCraftQuests() => CraftQuests;
     public int GetActiveQuestCount() => ActiveQuests.Count;
 
     public QuestBook()
     {
         ActiveQuests = new List<IQuestInstance>();
+        FetchQuests = new List<FetchQuestInstance>();
+        DefeatEnemyQuests = new List<DefeatEnemyQuestInstance>();
+        CraftQuests = new List<CraftQuestInstance>();
     }
     
     public void AddQuest(IQuestInstance questInstance)
     {
         ActiveQuests.Add(questInstance);
+        switch (questInstance)
+        {
+            case FetchQuestInstance fetchQuestInstance:
+                FetchQuests.Add(fetchQuestInstance);
+                break;
+            case DefeatEnemyQuestInstance defeatEnemyQuestInstance:
+                DefeatEnemyQuests.Add(defeatEnemyQuestInstance);
+                break;
+            case CraftQuestInstance craftQuestInstance:
+                CraftQuests.Add(craftQuestInstance);
+                break;
+        }
     }
 }
