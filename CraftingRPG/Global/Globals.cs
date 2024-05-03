aa9668076c3be83e26b8850f0ca1efa80e9f3bf9
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-05-02 20:21:07 -0400

    Added PlayerInfo to Globals.cs

--- CraftingRPG/Global/Globals.cs
+++ CraftingRPG/Global/Globals.cs
@@ -5,6 +5,7 @@ namespace CraftingRPG.Global;
 
 public static class Globals
 {
+    public static PlayerInfo PlayerInfo { get; set; }
     public static PlayerInstance Player { get; set; }
     public static  bool ActionKeyPressed { get; set; } = false;
     public static  OrthographicCamera Camera { get; set; }
