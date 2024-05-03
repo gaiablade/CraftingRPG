867379b787b452d4d86f5f6420c29f354183a627
classydragon <55965488+gaiablade@users.noreply.github.com>
2024-05-02 22:45:34 -0400

    Add LoadingZones to GameMap

--- CraftingRPG/MapManagement/GameMap.cs
+++ CraftingRPG/MapManagement/GameMap.cs
@@ -10,10 +10,11 @@ public class GameMap
     public int Height { get; set; }
     public int TileWidth { get; set; }
     public int TileHeight { get; set; }
-    public Dictionary<string, string> Properties { get; set; }
+    public IDictionary<string, string> Properties { get; set; }
     
-    public List<TileLayer> TileLayers { get; set; }
-    public List<ObjectLayer> ObjectLayers { get; set; }
-    public List<MapTileSet> TileSets { get; set; }
-    public List<IEnemyInstance> Enemies { get; set; }
+    public IList<TileLayer> TileLayers { get; set; }
+    public IList<ObjectLayer> ObjectLayers { get; set; }
+    public IList<MapTileSet> TileSets { get; set; }
+    public IList<IEnemyInstance> Enemies { get; set; }
+    public IList<LoadingZone> LoadingZones { get; set; }
 }
